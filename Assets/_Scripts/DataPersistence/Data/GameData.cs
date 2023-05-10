f02afffc9c749b21f29f15925f0a1818b2b5e454
Temporas <kys76123456@gmail.com>
2023-05-10 10:21:09 +0200

    Update GameData.cs

--- Assets/_Scripts/DataPersistence/Data/GameData.cs
+++ Assets/_Scripts/DataPersistence/Data/GameData.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 [System.Serializable]
 public class GameData : MonoBehaviour
 {
+    //In Game save data
 
+    //In Run save data
+    public bool isInRun;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public GameData()
+    {
+        isInRun = false;
+    }
 }
