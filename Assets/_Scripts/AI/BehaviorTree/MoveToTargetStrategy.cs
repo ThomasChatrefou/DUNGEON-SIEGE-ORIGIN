daa254e755ef32eeec6043d3945a7728f9236504
hmarseu <hmarseu@gaming.tech>
2023-05-10 17:06:59 +0200

    push end of day [ERROR]
    achtung ne lancez pas ça c'est en travaux

--- Assets/_Scripts/AI/BehaviorTree/MoveToTargetStrategy.cs
+++ Assets/_Scripts/AI/BehaviorTree/MoveToTargetStrategy.cs
@@ -13,33 +13,28 @@ public class MoveToTargetStrategy : IBehaviorTree
     private float _lastAttackTime;
 
     private IBehaviorNode _attackNode;
-    private IBehaviorNode _NavMeshMouvNode;
+    private IBehaviorNode _navMeshMouvNode;
 
     public MoveToTargetStrategy(Transform target, NavMeshAgent agent, float speed, float attackRange, float attackSpeed)
     {
-
         this._target = target;
-        this._agent = agent;
-        
+        this._agent = agent;       
         this._attackRange = attackRange;
         this._attackCooldown = attackSpeed;
-
         _attackNode = new MeleeAttackStrategy(target);
-        _NavMeshMouvNode = new NavMeshMove(target,agent,speed);
+        _navMeshMouvNode = new NavMeshMove(target,agent,speed);
     }
 
     public void Execute(Transform entityTransform)
     {
-
         if (_target != null)
-        {
-            
-            IBehaviorNode.NodeState mouvState = _NavMeshMouvNode.Execute();
+        {    
+            IBehaviorNode.NodeState mouvState = _navMeshMouvNode.Execute();
 
             float distanceToTarget = Vector3.Distance(entityTransform.position, _target.position);
             if (distanceToTarget <= _attackRange)
             {
-                mouvState = _NavMeshMouvNode.Stop();
+                mouvState = _navMeshMouvNode.Stop();
                 if (Time.time - _lastAttackTime >= _attackCooldown)
                 {
                     //Debug.Log(_agent.isStopped + " " + distanceToTarget);
--- Assets/_Scripts/AI/BehaviorTree/StayAtRangeStrategy.cs
+++ Assets/_Scripts/AI/BehaviorTree/StayAtRangeStrategy.cs
@@ -1,17 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 /*
 Stay at Range and auto attack 
 */
 public class StayAtRangeStrategy : IBehaviorTree
 {
+    private NavMeshAgent _agent;
     private Transform _target;
-    private float _range;
+    private float _attackrange;
     private GameObject _projectile;
     private float _speed;
-  public void Execute(Transform EntityTransform)
-    {
+    private float _attackCooldown;
+    private float _lastAttackTime;
 
+    private IBehaviorNode _attackNode;
+    private IBehaviorNode _navMeshMouvNode;
+
+    public StayAtRangeStrategy(Transform target,NavMeshAgent agent, float attackrange, GameObject projectile, float speed, float attackCooldown, float lastAttackTime)
+    {
+        this._agent = agent;
+        this._target = target;
+        this._attackrange = attackrange;
+        this._projectile = projectile;
+        this._speed = speed;
+        this._attackCooldown = attackCooldown;
+        this._lastAttackTime = lastAttackTime;
+
+        //_attackNode = new RangeAttackStrategy(target,projectile,);
+        _navMeshMouvNode = new NavMeshMove(target, agent, speed);
+    }
+
+    public void Execute(Transform EntityTransform)
+    {
+        if (_target != null)
+        {
+            IBehaviorNode.NodeState mouvState = _navMeshMouvNode.Execute();
+            float distanceToTarget = Vector3.Distance(EntityTransform.position, _target.position);
+            if (distanceToTarget >= _attackrange)
+            {
+                mouvState = _navMeshMouvNode.Stop();
+                if (Time.time - _lastAttackTime >= _attackCooldown)
+                {
+                    //IBehaviorNode.NodeState attackState = _attackNode.Execute();
+                    if (/*attackState == IbehaviorNode.NodeState.Success*/)
+                    {
+                        _lastAttackTime = Time.time;
+                    }
+                }
+            }
+            else if(distanceToTarget <_attackrange-0.5f)
+            {
+
+            }
+                
+          }
     }
 }
