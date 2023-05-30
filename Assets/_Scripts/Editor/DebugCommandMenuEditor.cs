70c126295552a5326b66eddda870aa704c77ca87
Asbel <tchatrefou@gaming.tech>
2023-05-31 00:03:04 +0200

    Add debug command Menu with kill all enemies command

--- Assets/_Scripts/Editor/DebugCommandMenuEditor.cs
+++ Assets/_Scripts/Editor/DebugCommandMenuEditor.cs
@@ -1,16 +1,42 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
-public class DebugCommandMenuEditor : MonoBehaviour
+public class DebugCommandMenuEditor : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Window/Debug Command Menu")]
+    public static void Init()
     {
-        
+        GetWindow(typeof(DebugCommandMenuEditor));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Debug Command Menu", EditorStyles.boldLabel);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Kill all enemies"))
+        {
+            KillAllEnemies();
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    private void KillAllEnemies()
+    {
+        int nbEnemiesKilled = 0;
+        foreach (GameObject go in EditorSceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            foreach (AIBaseController aiController in go.GetComponentsInChildren<AIBaseController>())
+            {
+                Destroy(aiController.gameObject);
+                ++nbEnemiesKilled;
+            }
+        }
+        Debug.Log("Command Kill all enemies : " + nbEnemiesKilled + " enemies killed");
     }
 }
