6266c1f400cf2e2e239b5dfa989b98ebd14c396d
hmarseu <hmarseu@gaming.tech>
2023-06-14 10:59:42 +0200

    Boss Undone

--- Assets/_Scripts/AI/BehaviorTree/Boss/ChooseAttRip.cs
+++ Assets/_Scripts/AI/BehaviorTree/Boss/ChooseAttRip.cs
@@ -1,17 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChooseAttRip : IBehaviorNode
 {
-    BlackBoard _blackBoard;
+    BlackBoard _blackboard;
+
+    NavMeshAgent _agent;
+    Transform _target;
+    Transform _entityTransform;
+    float _range;
+  
+    Vector3 _direction;
+    float _distanceToTarget;
+
+    float _cooldownAttack;
+    float _lastAction;
+
+    IBehaviorNode _attackNode;
+    IBehaviorNode _fightbackNode;
+    public ChooseAttRip(BlackBoard blackboard)
+    {
+        SetBlackBoard(blackboard);
+        _target = _blackboard.GetVariable<Transform>("target");
+        _agent = _blackboard.GetVariable<NavMeshAgent>("agent");
+        _cooldownAttack = _blackboard.GetVariable<float>("cooldownAttack");
+
+        /*
+         _attackNode = new 
+         _fightbackNode = new 
+         */
+    }
     public void Execute()
     {
-
+        int rand = Random.Range(0, 1);
+        if (rand == 1)
+        {
+            _attackNode.Execute();
+        }
+        else
+        {
+            _fightbackNode.Execute();
+        }
     }
     public bool Evaluate()
     {
-        return true;
+        _direction = _target.position - _entityTransform.position;
+        _direction.y = 0;
+        _distanceToTarget = _direction.magnitude;
+        
+        if (_distanceToTarget < _range)
+        {
+            if (Time.time - _lastAction >= _cooldownAttack)
+            {
+                _lastAction = Time.time;
+                return true;
+            }
+        }
+        
+        return false;
+        
     }
     public void Stop()
     {
--- Assets/_Scripts/AI/BehaviorTree/Boss/WalkToPlayer.cs
+++ Assets/_Scripts/AI/BehaviorTree/Boss/WalkToPlayer.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WalkToPlayer : IBehaviorNode
 {
+    BlackBoard _blackboard;
+    Transform _target;
+    NavMeshAgent _agent;
+    float _speed;
+
+    IBehaviorNode _mouvNode;
+
+    public WalkToPlayer(BlackBoard blackboard)
+    {
+        SetBlackBoard(blackboard);
+        _target = _blackboard.GetVariable<Transform>("target");
+        _agent = _blackboard.GetVariable<NavMeshAgent>("agent");
+        _speed = _blackboard.GetVariable<float>("speed");
+        _mouvNode = new NavMeshMove(_target, _agent, _speed);
+    }
     public void Execute()
     {
-
+        _mouvNode.Execute();
     }
     public bool Evaluate()
     {
@@ -18,6 +34,6 @@ public class WalkToPlayer : IBehaviorNode
     }
     public void SetBlackBoard(BlackBoard bb)
     {
-
+        _blackboard = bb;
     }
 }
