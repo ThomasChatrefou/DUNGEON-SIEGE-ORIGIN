5fb90d0e1d4b4c705b8783febead22c09c2005ba
Americanos33 <65153418+Americanos33@users.noreply.github.com>
2023-06-21 11:58:53 +0200

    Balance GD

--- Assets/_Scripts/Weapons/WeaponStatisticUgradeSO.cs
+++ Assets/_Scripts/Weapons/WeaponStatisticUgradeSO.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponStatisticUgrade", menuName = "ScriptableObjects/Weapons/WeaponStatisticUgrade")]
 public class WeaponStatisticUgradeSO : ScriptableObject
 {
-    public int Damages;
+    public float Damages;
     public float AttackSpeed;
     public float Range;
 }
