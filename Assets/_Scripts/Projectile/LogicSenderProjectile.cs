190853fd1b8978f335e70c9be9524151910b5b42
Americanos33 <65153418+Americanos33@users.noreply.github.com>
2023-06-21 10:34:54 +0200

    Fixing turret specs

--- Assets/_Scripts/Projectile/LogicSenderProjectile.cs
+++ Assets/_Scripts/Projectile/LogicSenderProjectile.cs
@@ -8,6 +8,9 @@ public class LogicSenderProjectile : MonoBehaviour
     [SerializeField] private string _targetTag;
     [Tag]
     [SerializeField] private string _senderTag;
+    [Layer]
+    [Label("Layer for hitable objects")]
+    [SerializeField] private string _hitableLayerName = "Hitable";
 
     private Rigidbody _rigidbody;
     private BaseAbilitySO _logicToSend;
@@ -25,6 +28,7 @@ public class LogicSenderProjectile : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    // Collision with ennemies with collision enter
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag(_senderTag)) return;
@@ -33,5 +37,22 @@ public class LogicSenderProjectile : MonoBehaviour
             _logicToSend.Use(ref _dataToSend);
         }
         Destroy(gameObject);
+    }
+
+    // Collision with ennemies shield with trigger enter
+    private void OnTriggerEnter(Collider other)
+    {
+        SphereCollider collider = other.GetComponent<SphereCollider>();
+        Debug.Log("SphereCollider Radius : " + collider.radius*other.transform.lossyScale.x);
+        if (collider != null)
+        {
+            int layer = 1 << LayerMask.NameToLayer(_hitableLayerName);
+            Collider[] hitColliders = Physics.OverlapSphere(other.transform.position, collider.radius*other.transform.lossyScale.x, layer);
+            Debug.Log("Length of Hitcollider : " + hitColliders.Length);
+            if (hitColliders.Length == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
